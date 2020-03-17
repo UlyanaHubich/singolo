8b06c773da8fe3fe11707075845779a2433a76aa
Ulyana Hubich <ulyana.hubich@gmail.com>
2020-03-17 16:33:27 +0300

    feat: add active menu links & frame around Portfolio images

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-/* HEADER part I*/
-
+window.onload = () => {
+    MenuActive();
+    PortfolioImageFrame();
+  }
+  /*Add active menu links*/
   
+  const MenuActive = () => {
+    const menuLinks = document.querySelectorAll('.menu-link');
+    menuLinks.forEach(item => {
+      item.addEventListener('click', (event) => {
+        menuLinks.forEach(link => link.classList.remove('menu-active'));
+        event.target.classList.add('menu-active');
+      })
+    });
+  }
+  
+  /*Add frame around Portfolio images*/
+  
+  const PortfolioImageFrame = () => {
+      const portfolioItems = document.querySelectorAll('.portfolio-item');
+      document.querySelector('.images-layot').addEventListener('click', (event) => {
+        if (event.target.classList.contains('portfolio-item')) {
+          portfolioItems.forEach(item => item.classList.remove('portfolio-item-active'))
+          event.target.classList.add('portfolio-item-active');
+        }
+      })
+    }
