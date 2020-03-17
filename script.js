f6d0ca8a71bf7dc63f9d293340386ef8a83789dd
Ulyana Hubich <ulyana.hubich@gmail.com>
2020-03-17 16:57:57 +0300

    feat: add active menu links in Portfolio

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
 window.onload = () => {
-    MenuActive();
-    PortfolioImageFrame();
-  }
-  /*Add active menu links*/
-  
-  const MenuActive = () => {
-    const menuLinks = document.querySelectorAll('.menu-link');
-    menuLinks.forEach(item => {
+  MenuActive();
+  PortfolioImageFrame();
+  TagPortfolioActive();
+}
+/*Add active menu links*/
+
+const MenuActive = () => {
+  const menuLinks = document.querySelectorAll('.menu-link');
+  menuLinks.forEach(item => {
+    item.addEventListener('click', (event) => {
+      menuLinks.forEach(link => link.classList.remove('menu-active'));
+      event.target.classList.add('menu-active');
+    })
+  });
+}
+
+/*Add frame around Portfolio images*/
+
+const PortfolioImageFrame = () => {
+    const portfolioItems = document.querySelectorAll('.portfolio-item');
+    document.querySelector('.images-layot').addEventListener('click', (event) => {
+      if (event.target.classList.contains('portfolio-item')) {
+        portfolioItems.forEach(item => item.classList.remove('portfolio-item-active'))
+        event.target.classList.add('portfolio-item-active');
+      }
+    })
+}
+
+/*Add active links in Portfolio*/
+
+const TagPortfolioActive = () => {
+    const tagLinks = document.querySelectorAll('.tag');
+    tagLinks.forEach(item => {
       item.addEventListener('click', (event) => {
-        menuLinks.forEach(link => link.classList.remove('menu-active'));
-        event.target.classList.add('menu-active');
+        tagLinks.forEach(link => link.classList.remove('tag-active'));
+        event.target.classList.add('tag-active');
       })
     });
   }
-  
-  /*Add frame around Portfolio images*/
-  
-  const PortfolioImageFrame = () => {
-      const portfolioItems = document.querySelectorAll('.portfolio-item');
-      document.querySelector('.images-layot').addEventListener('click', (event) => {
-        if (event.target.classList.contains('portfolio-item')) {
-          portfolioItems.forEach(item => item.classList.remove('portfolio-item-active'))
-          event.target.classList.add('portfolio-item-active');
-        }
-      })
-    }
